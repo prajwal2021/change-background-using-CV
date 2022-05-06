ba7f1bef8b62b0cccac7b99d1fb6b6b31363cac0
prajwal <71785710+prajwal2021@users.noreply.github.com>
2022-05-06 08:44:33 -0700

    Add files via upload

---
+++ chage_background.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+# variable 
+RED =255
+GREEN =255
+BLUE =255
+ALPHA = 200
+Image = cv.imread("Blue.png", cv.IMREAD_UNCHANGED)
+
+trasn_mask = Image[:,:,3 ]==0
+
+Image[trasn_mask]=[BLUE, GREEN, RED, ALPHA]
+cv.imwrite("output.png", Image)
+print(Image.shape)
+resized = cv.resize(Image, None, fx=0.5, fy=0.5)
+cv.imshow('windows', resized)
+cv.waitKey(0)
